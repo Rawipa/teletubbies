f37fba5b985a011794586c2689b9acfeb5a96f28
ppraewpanida <59070094@it.kmitl.ac.th>
2016-12-07 10:24:05 +0700

    add description

--- ave_northern.py
+++ ave_northern.py
@@ -1,13 +1,13 @@
 """Show Information"""
 import pygal #create graph
 from xlrd import open_workbook #import file from excel
-from pygal.style import LightColorizedStyle #style of graph(color)
+from pygal.style import BlueStyle #style of graph(color)
 def function():
     """Percent Average"""
 
     #open_worksheet
     book = open_workbook('D:/Book1.xlsx') #openfile in ---> (name of file)
-    sheet = book.sheet_by_index(0) #sheet cell
+    sheet = book.sheet_by_index(1) #sheet cell
     kill_honest = [sheet.cell(3, col_index).value for col_index in range(7, 8)] #sheet.cell(row, col_index) ,,,, range(column)
     kill_bad = [sheet.cell(4, col_index).value for col_index in range(7, 8)]
     careless = [sheet.cell(5, col_index).value for col_index in range(7, 8)]
@@ -16,7 +16,7 @@ def function():
     rape = [sheet.cell(8, col_index).value for col_index in range(7, 8)]
 
     #pie_chart
-    pie_chart = pygal.Pie(inner_radius=.4, fill=True, interpolate='cubic', style=LightColorizedStyle)
+    pie_chart = pygal.Pie(inner_radius=.4, fill=True, interpolate='cubic', style=BlueStyle)
     pie_chart.title = sheet.cell(0, 0).value
     pie_chart.add(sheet.cell(3, 0).value, sheet.cell(3, 7).value)
     pie_chart.add(sheet.cell(4, 0).value, sheet.cell(4, 7).value)
@@ -24,6 +24,6 @@ def function():
     pie_chart.add(sheet.cell(6, 0).value, sheet.cell(6, 7).value)
     pie_chart.add(sheet.cell(7, 0).value, sheet.cell(7, 7).value)
     pie_chart.add(sheet.cell(8, 0).value, sheet.cell(8, 7).value)
-    pie_chart.render_to_file('AverageThailand.svg')
+    pie_chart.render_to_file('AverageNorthern.svg')
 
 function()
